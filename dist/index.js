4fc5e002ca8693a4b4db200ca29a9c8175668dc3
Kerasiya Vishnu <vishnuk@ziguts.com>
2023-08-09 02:37:25 +0100

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -20,14 +29,16 @@ app.use((0, cors_1.default)({ origin: "*" }));
 // Routes
 app.use("/api", authorRoutes_1.default);
 // Connect to MongoDB
-mongoose_1.default
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-    console.log("Connected to MongoDB");
-})
-    .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
+const conncetDB = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield mongoose_1.default.connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB");
+    }
+    catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+    }
 });
+conncetDB();
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,15 @@ app.use(cors({ origin: "*" }));
 app.use("/api", authorRoutes);
 
 // Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URL!)
-  .then(() => {
+const conncetDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error("Error connecting to MongoDB:", err);
-  });
+  }
+};
+conncetDB();
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
